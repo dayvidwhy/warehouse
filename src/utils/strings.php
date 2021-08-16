7a14c14b39b62f47a4bbecf07d77956aabf88ef9
dayvidwhy <dayvidwhy@gmail.com>
2021-08-17 00:33:04 +1000

    Types for string functions.

--- src/utils/strings.php
+++ src/utils/strings.php
@@ -1,17 +1,17 @@
 <?php
 // converst normal words to kebab-case
-function convertToSlug ($normal) {
+function convertToSlug (string $normal): string {
     return strtolower(implode('-', explode(' ', $normal)));
 }
 
 // converts kebab-case to normal words
-function convertToNormal ($slug) {
+function convertToNormal (string $slug): string {
     $slug = strtolower($slug);
     return ucwords(implode(' ', explode('-', $slug)));
 }
 
 // checks that a string ends with another string
-function endsWith($haystack, $needle) {
+function endsWith(string $haystack, string $needle): string {
     return $needle === "" || (substr($haystack, -strlen($needle)) === $needle);
 }
 ?>
