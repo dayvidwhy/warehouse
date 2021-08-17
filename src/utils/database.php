6f0656e3e4576740fa111089204cafbc647ea46d
dayvidwhy <dayvidwhy@gmail.com>
2021-08-17 15:58:23 +1000

    heredoc for sql queries.

--- src/utils/database.php
+++ src/utils/database.php
@@ -34,7 +34,7 @@ class Database {
     // reproduce the stock table with correct format
     function recreateStockTable () {
         $this->link->query("DROP TABLE IF EXISTS stock");
-        $this->link->query("
+        $this->link->query(<<<EOL
             CREATE TABLE IF NOT EXISTS stock (
                 stock_id varchar(40) NOT NULL,
                 stock_name varchar(40) NOT NULL,
@@ -44,17 +44,30 @@ class Database {
                 in_stock int(11) DEFAULT NULL,
                 PRIMARY KEY (stock_id)
             ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-        ");
+        EOL);
     }
 
     function addStock ($stockId, $stockName, $imageDir, $stockStory, $inStock) {
-        $query = $this->link->prepare("INSERT INTO stock (stock_id, stock_name, image_path, stock_story, image_large, in_stock) VALUES (?, ?, ?, ?, ?, ?)");
+        $query = $this->link->prepare(<<<EOL
+            INSERT INTO stock (
+                stock_id,
+                stock_name,
+                image_path,
+                stock_story,
+                image_large,
+                in_stock
+            ) VALUES (?, ?, ?, ?, ?, ?)
+        EOL);
         $query->bind_param("ssssss", $stockId, $stockName, $imageDir, $stockStory, $imageDir, $inStock);
         $query->execute();
     }
 
     function fetchStock ($stockName) {
-        $query = $this->link->prepare("SELECT * FROM stock WHERE (stock_name = ?) AND (in_stock != 0)");
+        $query = $this->link->prepare(<<<EOL
+            SELECT * FROM stock
+            WHERE (stock_name = ?)
+            AND (in_stock != 0)
+        EOL);
         $query->bind_param("s", $stockName);
         $query->execute();
         $rows = $query->get_result()->fetch_all(MYSQLI_ASSOC);
@@ -63,7 +76,11 @@ class Database {
 
     function fetchCategories () {
         // initiate query
-        $query = $this->link->prepare("SELECT stock_name, MAX(image_path) FROM stock WHERE (in_stock != 0) GROUP BY stock_name");
+        $query = $this->link->prepare(<<<EOL
+            SELECT stock_name, MAX(image_path) FROM stock
+            WHERE (in_stock != 0)
+            GROUP BY stock_name
+        EOL);
         $query->execute();
         $rows = $query->get_result()->fetch_all(MYSQLI_ASSOC);
         return $rows;
